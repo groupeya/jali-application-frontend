4a028fd7443e000d3e0ce6212a1bbcde9767ee75
Herve <henzy80@hotmail.com>
2024-05-23 09:18:56 +0200

    Fetch address villages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import RegisterApplicant from './pages/application'
+import Signup from './pages/signup'
+
 function App() {
 
   return (
     <>
     <div className="container">
-      <div className="flex justify-between align-top">
-          <div className='sticky top-0 h-24'>
-          <img src="./Logo-02.png" alt="alt" width={350} height={350} />
-        </div>
+      <div className="flex justify-center align-top">
           <div className=''>
             <RegisterApplicant />
+            {/* <Signup /> */}
         </div>
       </div>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,22 +5,40 @@ interface InputProps {
   name: string;
   value: string | number | boolean;
   type?: string;
+  options?: string[]
   handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ label, name, value, type = 'text', handleChange, placeholder }) => {
+const Input: React.FC<InputProps> = ({ label, name, value, type = 'text', handleChange, placeholder, options }) => {
   return (
     <div className='md:flex md:items-center mb-6'>
       <div className='md:w-1/3'>
         <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">{label}</label>
       </div>
-      {type === 'checkbox' ? (
+
+      {type === 'select' ? (
+        <select
+          name={name}
+          value={value as string}
+          onChange={handleChange}
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-700"
+        >
+          <option value="" disabled selected>
+            Hitamo
+          </option>
+          {options?.map(option => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : type === 'checkbox' ? (
         <input
           type="checkbox"
           name={name}
           checked={Boolean(value)}
           onChange={handleChange}
-          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          className=""
         />
       ) : (
         <input
@@ -28,8 +46,8 @@ const Input: React.FC<InputProps> = ({ label, name, value, type = 'text', handle
           name={name}
           value={value as string | number}
           onChange={handleChange}
-            className="bg-white appearance-none border-2 border-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-00"
-            placeholder={placeholder}
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-700"
+          placeholder={placeholder}
         />
       )}
     </div>
