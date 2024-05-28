9a3358bd2c389e3234b8d55491c80638cce7c7ea
Herve <henzy80@hotmail.com>
2024-05-28 04:21:11 +0200

    Add Cells/Villages

--- src/components/AddressSelectComponentProps.tsx
+++ src/components/AddressSelectComponentProps.tsx
@@ -23,6 +23,7 @@ const fetchDistricts = async (provinceId: number, apiUrl: string) => {
   return response.json();
 };
 
+
 const fetchSectors = async(districtId: number, apiUrl: string) => {
   const response = await fetch(`${apiUrl}/districts/${districtId}/sectors`);
   console.log(response)
@@ -32,6 +33,25 @@ const fetchSectors = async(districtId: number, apiUrl: string) => {
   return response.json();
 }
 
+
+const fetchCells = async (sectorId: number, apiUrl: string) => {
+  const response = await fetch(`${apiUrl}/sectors/${sectorId}/cells`);
+  console.log(response)
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+}
+
+const fetchVillages = async (cellId: number, apiUrl: string) => {
+  const response = await fetch(`${apiUrl}/cells/${cellId}/villages`);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+
 const AddressSelectComponent: React.FC<AddressSelectComponentProps> = ({
   onAddressSelect,
   label,
@@ -40,6 +60,8 @@ const AddressSelectComponent: React.FC<AddressSelectComponentProps> = ({
   const [selectedProvince, setSelectedProvince] = useState<number | null>(null);
   const [selectedDistrict, setSelectedDistricts] = useState<number | null>(null);
   const [selectedSector, setSelectedSector] = useState<number | null>(null)
+  const [selectedCell, setSelectedCell] = useState<number | null>(null);
+  const [selectedVillage, setSelectedVillage] = useState<number | null>(null);
 
   const { data: provinces, error: provinceError, isLoading: provinceLoading } = useQuery({
     queryKey: ['provinces'],
@@ -66,7 +88,24 @@ const AddressSelectComponent: React.FC<AddressSelectComponentProps> = ({
     enabled: !!selectedDistrict,
   });
 
- console.log(sectorsData)
+  const { data: cellsData, error: cellsError, isLoading: cellLoading } = useQuery({
+    queryKey: ['cells', selectedSector],
+    queryFn: () =>
+      selectedSector
+        ? fetchCells(selectedSector, apiUrl)
+        : Promise.resolve({ cells: [] }),
+    enabled: !!selectedSector,
+  });
+
+  const { data: villagesData, error: villagesError, isLoading: villageLoading } = useQuery({
+    queryKey: ["villages", selectedCell],
+    queryFn: () =>
+      selectedCell
+        ? fetchVillages(selectedCell, apiUrl)
+        : Promise.resolve({ villages: [] }),
+    enabled: !!selectedCell,
+  });
+ console.log(cellsData)
 
   const handleProvinceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedId = parseInt(e.target.value, 10);
@@ -83,6 +122,18 @@ const AddressSelectComponent: React.FC<AddressSelectComponentProps> = ({
   const handleSectorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedId = parseInt(e.target.value, 10);
     setSelectedSector(selectedId);
+    onAddressSelect(selectedId);
+  };
+
+  const handleCellhange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = parseInt(e.target.value, 10);
+    setSelectedCell(selectedId);
+    onAddressSelect(selectedId);
+  };
+
+  const handleVillageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = parseInt(e.target.value, 10);
+    setSelectedVillage(selectedId);
     onAddressSelect(selectedId);
   };
 
@@ -114,6 +165,28 @@ const AddressSelectComponent: React.FC<AddressSelectComponentProps> = ({
             sectorsData.sectors.map((sector: { id: number; name: string }) => (
               <option key={sector.id} value={sector.id}>
                 {sector.name}
+              </option>
+            ))}
+        </select>
+      )}
+      {selectedSector !== null && (
+        <select onChange={handleCellhange} disabled={cellLoading}>
+          <option value="">Select Cell</option>
+          {cellsData &&
+            cellsData.cells.map((cell: { id: number; name: string }) => (
+              <option key={cell.id} value={cell.id}>
+                {cell.name}
+              </option>
+            ))}
+        </select>
+      )}
+      {selectedCell !== null && (
+        <select onChange={handleVillageChange} disabled={villageLoading}>
+          <option value="">Select Village</option>
+          {villagesData &&
+            villagesData.villages.map((village: { id: number; name: string }) => (
+              <option key={village.id} value={village.id}>
+                {village.name}
               </option>
             ))}
         </select>
