8cbf6b1406647ded6f86dd98726f1efe59693d29
Herve <henzy80@hotmail.com>
2024-05-31 15:46:27 +0200

    production

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -54,7 +54,7 @@ interface FormData {
 
 
 const RegisterApplicant = () => {
-  const BaseUrl = "http://localhost:7800/api/v1";
+  const BaseUrl = "http://159.223.111.104:7800/api/v1";
 
   const fetchMotos = async () => {
     const response = await fetch(`${BaseUrl}/motoleasing`)
